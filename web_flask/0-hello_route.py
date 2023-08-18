93b0d03957b529a7378e3422fc72bd47e883376c
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-08-18 11:34:22 -0700

    add documentation in module

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" a script that starts a Flask web application """
 from flask import Flask
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def hello():
+    """a simple function to return text """
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-	app.run()
+    app.run()
